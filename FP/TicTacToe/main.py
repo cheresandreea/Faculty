619db216d8c232e91c81fa96f8ae0c498e6e2e52
912-Cheres-Andreea <115451911+cheresandreea@users.noreply.github.com>
2024-01-31 15:28:44 +0200

    updated

--- FP/TicTacToe/main.py
+++ FP/TicTacToe/main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+"""
+1. Modular programming
+2. Test-driven development
+3. Exceptions
+4. command-driven UI
+Tic Tac Toe - human vs. computer
+    play 5 (5th square on the board)
+    play top middle
+    play 1,1
+    play B2
+"""
