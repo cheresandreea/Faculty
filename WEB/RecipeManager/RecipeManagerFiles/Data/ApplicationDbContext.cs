634d73934eb7771533172a79024aba3fed25797a
922-Cheres-Andreea <115451911+cheresandreea@users.noreply.github.com>
2024-06-20 21:40:21 +0300

    Add files via upload

---
+++ WEB/RecipeManager/RecipeManagerFiles/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RecipeManager.Models;
+using System.Collections.Generic;
+
+namespace RecipeManager.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
---
+++ WEB/RecipeManager/RecipeManagerFiles/Models/Recipe.cs
@@ -0,0 +1,12 @@
+﻿namespace RecipeManager.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Ingredients { get; set; }
+        public string Instructions { get; set; }
+    }
+}
